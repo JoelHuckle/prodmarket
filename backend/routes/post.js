8622191f54688da50901d42a9f0f75de2a474df5
Joel Huckle <133560905+JoelHuckle@users.noreply.github.com>
2024-04-07 09:38:24 +0100

    Delete backend directory

--- backend/routes/post.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const postController = require("../controllers/post");
-
-router.post("/createPost", postController.createPost);
-
-module.exports = router;
